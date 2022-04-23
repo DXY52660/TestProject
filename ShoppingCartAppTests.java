27b7cd60fdbf39a56fda4db1967ac180d22d06a2
Divya <104269286+DXY52660@users.noreply.github.com>
2022-04-23 14:50:11 -0500

    Add files via upload

---
+++ ShoppingCartAppTests.java
@@ -0,0 +1,16 @@
+<dl>
+  <dt> First definition</dt>  <dd>  First explanation</dd>
+  <dt> Second definition</dt> <dd> Second explanation</dd>
+</dl>package ShoppingCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
